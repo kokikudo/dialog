257dd5cf6f2cd2a67eed2d04d637d23f8456b7fe
kokikudo <massanmesu@icloud.com>
2021-12-28 14:15:33 +0900

    自作ダイアログ実装

--- app/src/main/java/com/example/dialog/MyAlertDialogFragment.kt
+++ app/src/main/java/com/example/dialog/MyAlertDialogFragment.kt
@@ -1,20 +1,41 @@
 package com.example.dialog
+
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import java.lang.IllegalStateException
 
-class MyAlertDialogFragment : DialogFragment(){
+class MyAlertDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
         val dialog = activity?.let {
-            val inputText = arguments?.getString("inputText")
+
+            // ダイアログレイアウトファイルをViewに変換
+            val layout = LayoutInflater.from(it).inflate(R.layout.dialog_body, null)
+
+            // ダイアログのBody部分であるTextViewに保存した文字列を代入
+            val textMsg = layout.findViewById<TextView>(R.id.textView)
+            textMsg.text = arguments?.getString("inputText")
+
+            // ダイアログ実装
             AlertDialog.Builder(it).apply {
                 setTitle(R.string.d_title)
-                setMessage("こんにちは、${inputText}さん")
+                setView(layout)
                 setIcon(R.drawable.wings)
-
+                setNegativeButton("いいえ") { _, _ ->
+                    Toast.makeText(activity, "キャンセルしました。", Toast.LENGTH_SHORT).show()
+                }
+                setPositiveButton("はい") { _, _ ->
+                    Toast.makeText(
+                        activity,
+                        getString(R.string.greet, textMsg.text),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }.create() // 生成するcreateメソッドを忘れない
         }
 
