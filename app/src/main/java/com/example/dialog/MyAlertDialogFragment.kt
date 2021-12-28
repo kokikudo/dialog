6c87eb06f7c395e67f817fcfae6d2a9522d43d7b
kokikudo <massanmesu@icloud.com>
2021-12-28 11:46:21 +0900

    チェックボックスで実装

--- app/src/main/java/com/example/dialog/MyAlertDialogFragment.kt
+++ app/src/main/java/com/example/dialog/MyAlertDialogFragment.kt
@@ -9,23 +9,35 @@ import java.lang.IllegalStateException
 
 class MyAlertDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        var selectedItemIndex = 0 // 選択した項目のIndex
-        val items = arrayOf("A型", "B型", "O型", "AB型") // ダイアログに表示したい項目のリスト
+        val items = arrayOf("チャーハン", "ラーメン", "たこ焼き", "焼きそば", "キャベツ") // ダイアログに表示したい項目のリスト
+        val selectedItems = booleanArrayOf(true, true, true, false, false, false) // 各項目の選択状況
         val dialog = activity?.let {
             AlertDialog.Builder(it).apply {
-                setTitle(R.string.blood_type_title)
+                setTitle(R.string.favarite_food_title)
                 setIcon(R.drawable.wings)
 
-                    //選択したらselectedItemIndexをその項目のIndex番号に更新
-                setSingleChoiceItems(items, selectedItemIndex) { dialog, which ->
-                    selectedItemIndex = which
+                    //各項目の選択状況が変更される
+                setMultiChoiceItems(items, selectedItems) { dialog, which, isChecked ->
+                    selectedItems[which] = isChecked
                 }
 
                     //トースト
-                    // 要は選択した項目のIndexを変数で補完し、最後に参照するという古典的なやり方
+                    //trueの項目だけを入れたリストを作る
                 setPositiveButton("OK") { dialog, which ->
+                    val favaliteFood = mutableListOf<String>()
+                        // forEachIndexed: リストの各要素とIndexを同時に取得してくる
+                    selectedItems.forEachIndexed { index, e ->
+                        if (e) favaliteFood.add(items[index])
+                    }
+
+                    val msg: String = if (favaliteFood.isNotEmpty()) {
+                        "好きな食べ物は${favaliteFood.joinToString()}です"
+                    } else {
+                        "好きな食べ物はありません。"
+                    }
+
                     Toast.makeText(
-                        activity, "「${items[selectedItemIndex]}」が選択されました。",
+                        activity, msg,
                         Toast.LENGTH_SHORT
                     ).show()
                 }
