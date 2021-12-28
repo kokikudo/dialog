5341be1b59d86d81605002e14daae2cb65931821
kokikudo <massanmesu@icloud.com>
2021-12-28 16:01:16 +0900

    動的なメニューを作成

--- app/src/main/java/com/example/dialog/MainActivity.kt
+++ app/src/main/java/com/example/dialog/MainActivity.kt
@@ -7,11 +7,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import com.example.dialog.databinding.ActivityMainBinding
 
-// アクションバーからアイコンをタップしたときに表示するメニューをオプションメニューと呼ぶ
-// あらかじめ設定ファイルで用意する静的なやり方と、コードからメニューを生成する動的な方法がある
-// 静的なやり方
-//　- アイコンを用意
-// - 定義ファイル作成
+// 動的なメニューは定義ファイルでつくるデータをonCreateOptionsMenuで書くだけ
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -34,9 +30,28 @@ class MainActivity : AppCompatActivity() {
 
     // 定義ファイルからオプションメニュー生成
     // メニューが初回表示されるときに実行されるActivityメソッド
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // メニューフレームを初期化しレイアウトをセット
-        menuInflater.inflate(R.menu.option_menu, menu)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        // ホームメニュー
+        menu.add(0, 0, 0, R.string.home).setIcon(R.drawable.menu1)
+
+        // サブメニュー
+        val subMenu1 = menu.addSubMenu(0, 1, 1, R.string.help).setIcon(R.drawable.menu2)
+        // helpの傘下メニュー
+        subMenu1.add(0, 2, 0, R.string.search).setIcon(R.drawable.menu3)
+        subMenu1.add(0, 3, 1, R.string.`in`).setIcon(R.drawable.menu4)
+        subMenu1.add(0, 4, 2, R.string.out).setIcon(R.drawable.menu5)
+        subMenu1.add(0, 5, 3, R.string.version).setIcon(R.drawable.menu6)
+
+        // サブメニュー
+        val subMenu2 = menu.addSubMenu(0, 6, 2, R.string.setting).setIcon(R.drawable.menu7)
+        // settingの傘下メニュー
+        subMenu2.add(0, 7, 0, R.string.bulb).setIcon(R.drawable.menu8)
+        subMenu2.add(0, 8, 1, R.string.speaker).setIcon(R.drawable.menu9)
+        subMenu2.add(0, 9, 2, R.string.key).setIcon(R.drawable.menu10)
+
+        // ゴミ箱
+        menu.add(0, 10, 3, R.string.garbage).setIcon(R.drawable.menu11)
         return true
     }
 
