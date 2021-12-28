23f273f35ac68100b35b6fd144f769bcbeee5afe
kokikudo <massanmesu@icloud.com>
2021-12-28 13:41:48 +0900

    時刻を指定するダイアログ実装

--- app/src/main/java/com/example/dialog/MyAlertDialogFragment.kt
+++ app/src/main/java/com/example/dialog/MyAlertDialogFragment.kt
@@ -2,6 +2,7 @@ package com.example.dialog
 
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
@@ -11,33 +12,35 @@ import java.util.*
 class MyAlertDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
-        // 選択した日付をEditTextに反映させる
+        // 選択した時刻をEditTextに反映させる
 
-        // 今日の日付
+        // 今の時刻を用意
         val todayCal = Calendar.getInstance()
         val dialog = activity?.let {
 
-            // DatePickerDialog
+            // TimePickerDialog
             // 第一引数にcontext(今回の場合は表示するActivity)
             // 第二引数に選択した時のイベントリスナー
-            // 第三、四、五はそれぞれ年、月、日の初期値を入れる
-            DatePickerDialog(
+            // 第三、四それぞれ時間、分の初期値を入れる
+            // 第五に24時間表記にするかどうか
+            TimePickerDialog(
                 it,
-                { view, year, monthOfYear, dayOfMonth ->
+                { view, hourOfDate, minute ->
                     // 選択した日付をEditTextに反映
                     val txtDate = it.findViewById<EditText>(R.id.editText)
                     txtDate.setText(
                         getString(
-                            R.string.date_text,
-                            year, monthOfYear + 1,
-                            dayOfMonth
+                            R.string.time_text,
+                            hourOfDate,
+                            minute
                         )
                     )
                 },
-                // 初期の日付
-                todayCal[Calendar.YEAR],
-                todayCal[Calendar.MONTH],
-                todayCal[Calendar.DAY_OF_MONTH],
+                // 初期の時刻
+                todayCal[Calendar.HOUR_OF_DAY],
+                todayCal[Calendar.MINUTE],
+                // 24時間表記にするか
+                true
             )
         }
 
